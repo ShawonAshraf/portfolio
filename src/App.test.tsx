00be60e600c44b8dbf8a673c3defb10bf7a78480
Shawon Ashraf <shawon13@live.com>
2020-01-08 15:17:06 +0100

    app test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders the terminal', () => {
+  window.HTMLElement.prototype.scrollIntoView = function() {};
+  const dom = render(<App />);
+
+  const terminal = dom.container.getElementsByClassName('terminal');
+  expect(terminal).toBeTruthy();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ import {
 class App extends Component<{}, {}> {
     render() {
         return (
-            <div
+            <div className="terminal"
                 style={{
                     display: "flex",
                     justifyContent: "center",
